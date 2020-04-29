2574f17342b61881742d9414116ebbca9c8a0fcf
koreantique <koreantique_@naver.com>
2020-04-29 16:10:09 +0900

    Daily commit

--- BFS_1697_숨바꼭질.cpp
+++ BFS_1697_숨바꼭질.cpp
@@ -1,1 +1,54 @@
 #include <iostream>
+#include <queue>
+
+using namespace std;
+
+
+int n, k;
+int d[100001];
+int check[100001] = {0,};
+
+void bfs(int x);
+
+int main(){
+    cin >> n >> k;
+    bfs(n);
+    cout << check[k]-1 <<endl;
+}
+
+void bfs(int x){
+    queue<int> q;
+    q.push(x);
+    check[x] = 1;
+
+    while(!q.empty()){
+        x = q.front();
+        q.pop();
+
+        //x+1, x-1, x*2에 대해서
+
+        int nx = x+1;
+        if( 0 <= nx && nx <= 100000){
+            if(check[nx] == 0){
+                q.push(nx);
+                check[nx] = check[x] + 1;
+            }
+        }
+
+        nx = x-1;
+        if( 0 <= nx && nx <= 100000){
+            if(check[nx] == 0){
+                q.push(nx);
+                check[nx] = check[x] + 1;
+            }
+        }
+
+        nx = x*2;
+        if( 0 <= nx && nx <= 100000){
+            if(check[nx] == 0){
+                q.push(nx);
+                check[nx] = check[x] + 1;
+            }
+        }
+    }
+}
